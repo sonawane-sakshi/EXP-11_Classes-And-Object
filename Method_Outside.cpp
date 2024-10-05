ccf454491ce71742429bcab90b2dc33f036aded3
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-05 22:33:35 +0530

    Add files via upload

---
+++ Method_Outside.cpp
@@ -0,0 +1,15 @@
+//Sakshi Sonawane 
+//PRN:24070123505
+#include <iostream>
+using namespace std;
+class Student{
+    public:
+    void display();
+};
+void Student::display(){
+    cout<<"I'm studying in second year.";
+}
+int main(){
+    Student s1;
+    s1.display();
+}
